38ad0732cadee7893a8a9f279190d8046a5d4b0a
kadts <kadtsynakate88@gmail.com>
2026-04-25 19:13:04 +0200

    hm_7

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,54 @@
-const key = new Key();
+class Key {
+  private signature: number = Math.random();
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-house.openDoor(person.getKey());
+class Person {
+  constructor(private key: Key) {}
 
-house.comeIn(person);
+  getKey(): Key {
+    return this.key;
+  }
+}
 
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Людина зайшла в будинок.');
+    } else {
+      console.log('Двері зачинені! Зайти неможливо.');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Ключ підійшов. Двері відчинено.');
+    } else {
+      console.log('Ключ не підходить! Двері залишаються зачиненими.');
+    }
+  }
+}
+
+// Сценарій життя
+const key = new Key(); // Створюємо ключ із випадковим підписом
+const house = new MyHouse(key); // Створюємо будинок, який відкривається цим ключем
+const person = new Person(key); // Даємо цей самий ключ людині
+
+house.openDoor(person.getKey()); // Людина намагається відкрити двері своїм ключем
+house.comeIn(person); // Людина заходить, якщо двері відкрилися
 
 export {};
